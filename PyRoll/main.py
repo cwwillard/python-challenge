a1acbd59556818dbcb7412085cd4d8fb84a09094
Carlisle Willard <cwalkerwillard@gmail.com>
2020-09-19 15:37:15 -0500

    Added file write to pyroll

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -17,20 +17,39 @@ with open(file_path) as csvfile:
             candidate_list[row[2]] +=1
         else:
             candidate_list[row[2]] = 1
-            print(row[2])
+            
 
-print(f"Total Number of Votes: {myTotalNum_votes}")
+
 
 Election_winner = ""
 current_top_votes = 0
 
+print("Election Results")
+print("-----------------------")
+print(f"Total Number of Votes: {myTotalNum_votes}")
+print("-----------------------")
+
 for candidate in candidate_list:
     if candidate_list[candidate] > current_top_votes:
-        Election_winner = candidate
-        current_top_votes = candidate_list[candidate]
+            Election_winner = candidate
+            current_top_votes = candidate_list[candidate]
 
+
+output_path = os.path.join("Analysis", "election_analysis.csv")
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(["Candidate", "Percentage Vote Won", "Number of Votes", "Total Number of Votes", "Winner"])
+    for myCandidate in candidate_list:
+        percent_vote = 100*candidate_list[myCandidate]/myTotalNum_votes
+        formatted_percent_vote = "{:.3f}".format(percent_vote)
+        print(f"{myCandidate}: {formatted_percent_vote}% ({candidate_list[myCandidate]})")
+        csvwriter.writerow([myCandidate, formatted_percent_vote, candidate_list[myCandidate], myTotalNum_votes, Election_winner])
+
+        
+
+print("-----------------------")
 print(f"Election Winner: {Election_winner}")
-print(current_top_votes)
+print("-----------------------")
 
 
             
